0285232ed76c0f6f2e1d3562b7ab8a26b8df7f33
hckang80 <hckang80@gmail.com>
2026-07-10 14:19:53 +0900

    chore: 글자 변경 테스트

--- src/app/(dashboard)/calendar/__client.container.tsx
+++ src/app/(dashboard)/calendar/__client.container.tsx
@@ -215,7 +215,7 @@ export default function CalendarClientContainer() {
   return (
     <div>
       <Heading as='h2' mb='4' size='7'>
-        일정확인
+        일정확인1
       </Heading>
       <div className={styles.root}>
         <div className={styles.legend}>
